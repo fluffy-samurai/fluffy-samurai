98c2342d09df1d4307fcacc69cd4fd2b10dacff6
Nia Watts <niawatts95@gmail.com>
2019-10-05 15:39:07 -0600

    Starting new site

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
-import App from './app/app.js'
+import { App } from './app/app.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
