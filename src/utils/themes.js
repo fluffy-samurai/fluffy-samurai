1e3c9ca267c18f4e349a1d697080e8cb78a1983b
Nia Watts <niawatts95@gmail.com>
2019-10-05 19:38:10 -0600

    Add light theme code
    Add a theme for light theme in case a lighter theme ever needs to be
    supported.

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -6,3 +6,12 @@ export const baseTheme = {
     background: 'darkslategray',
   },
 }
+
+// export const lightTheme = {
+//   spacing: 8,
+//   colors: {
+//     text: 'black',
+//     link: 'purple',
+//     background: 'lemonchiffon',
+//   },
+// }
